025a014f66095edc9a1eb88dd319e8f78d8a0585
Larry Bislew <bislewl@gmail.com>
2015-09-29 13:51:47 -0500

    update installer to use version numbering

--- catalog/YOUR_ADMIN/includes/installers/coupon_import_tool/1_1_0.php
+++ catalog/YOUR_ADMIN/includes/installers/coupon_import_tool/1_1_0.php
@@ -1,7 +1,4 @@
 <?php
-  if (!defined('IS_ADMIN_FLAG')) {
-    die('Illegal Access');
-  }
   $zc150 = (PROJECT_VERSION_MAJOR > 1 || (PROJECT_VERSION_MAJOR == 1 && substr(PROJECT_VERSION_MINOR, 0, 3) >= 5));  
   if ($zc150) { // continue Zen Cart 1.5.0
     // add configuration menu
