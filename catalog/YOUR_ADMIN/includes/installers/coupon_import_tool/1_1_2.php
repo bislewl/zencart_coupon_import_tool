ac0194a344f839fd2337b877fd1d6a0faf43f64f
Larry Bislew <bislewl@gmail.com>
2015-09-29 14:14:19 -0500

    Fixed Configuration Lang defines for Configuration page

---
+++ catalog/YOUR_ADMIN/includes/installers/coupon_import_tool/1_1_2.php
@@ -0,0 +1,3 @@
+<?php
+
+// updated uninstall.sql to remove configuration admin page and fixed installer defines, as well as improve name in configuration menu
--- uninstall.sql
+++ uninstall.sql
@@ -6,3 +6,4 @@ LIMIT 1;
 DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id AND configuration_group_id <> 0;
 DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id AND configuration_group_id <> 0;
 DELETE FROM admin_pages WHERE page_key = 'toolsCouponImportTool';
+DELETE FROM admin_pages WHERE page_key = 'configCouponImportTool';
